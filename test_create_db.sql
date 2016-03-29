752fd0a59e5d412d190ed4e8bf2f008d7559c385
ashavelev <1992Samsung>
2016-03-29 16:48:48 +0200

    new table + grants

--- test_create_db.sql
+++ test_create_db.sql
@@ -1,8 +1,11 @@
+CREATE USER owner WITH password 'd41d8cd98f00b204e9800998ecf8427e';
+
 CREATE TABLE lessons (
 	id serial NOT NULL,
 	name character varying(255) NOT NULL,	
 	CONSTRAINT id_lessons_pkey PRIMARY KEY (id)
 );
+
 
 CREATE TABLE lessons_time (
 	id serial NOT NULL,
@@ -22,6 +25,7 @@ CREATE TABLE shedule (
 	lesson_id integer NOT NULL,
 	group_id integer NOT NULL,
 	day smallint NOT NULL,
+	shedule_period smallint NOT NULL,
 	CONSTRAINT id_shedule_pkey PRIMARY KEY (id)
 );
 
@@ -29,7 +33,21 @@ CREATE TABLE admins (
 	id serial NOT NULL,
 	login character varying(255) NOT NULL,
 	password character varying(20) NOT NULL,
-	CONSTRAINT id_dmins_pkey PRIMARY KEY (id)
+	CONSTRAINT id_admins_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE shedule_periods (
+	id serial NOT NULL,
+	start_date timestamp,
+	end_date timestamp,
+	CONSTRAINT id_shedule_periods_pkey PRIMARY KEY (id)
+);
+
+GRANT ALL ON lessons TO owner;
+GRANT ALL ON lessons_time TO owner;
+GRANT ALL ON groups TO owner;
+GRANT ALL ON shedule TO owner;
+GRANT ALL ON admins TO owner;
+GRANT ALL ON shedule_periods TO owner;
 
 
